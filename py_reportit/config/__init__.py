c9e8889d2594309d588c5c8ffcd0bb88b42f82a7
Federico Gentile <gentfede+git@gmail.com>
2021-08-13 17:02:51 +0200

    feat allow for dev/prod config switch

--- py_reportit/config/__init__.py
+++ py_reportit/config/__init__.py
@@ -1,9 +1,13 @@
-import os
+import os, sys
 
 from dotenv import dotenv_values
 
+is_dev = "--dev" in sys.argv
+dotenv_variant = "dev" if is_dev else "prod"
+
 config = {
-    **dotenv_values(".secrets.dev.env"),
-    **dotenv_values(".shared.dev.env"),
+    **dotenv_values(f".secrets.{dotenv_variant}.env"),
+    **dotenv_values(f".shared.{dotenv_variant}.env"),
     **os.environ,
+    "DEV": is_dev,
 }
