6a6ab2f6fe1801d8ca14b174ec0ff9898b69621b
Federico Gentile <gentfede+git@gmail.com>
2022-01-15 00:26:44 +0100

    feat: add basic proxy support

--- py_reportit/shared/config/requests_session.py
+++ py_reportit/shared/config/requests_session.py
@@ -3,12 +3,26 @@ import logging
 
 from typing import Iterable
 from requests.sessions import Session
+from string import Template
 
 logger = logging.getLogger(f"py_reportit.{__name__}")
 
 def get_requests_session(config: dict) -> Iterable[Session]:
     with requests.Session() as session:
         logger.debug("Opening requests-session")
+
+        if config.get("USE_PROXY", False):
+            logger.debug("Setting proxy ...")
+            proxy_url = Template(config.get("PROXY_URL")).substitute({
+                "PROXY_SCHEME": config.get("PROXY_SCHEME"),
+                "PROXY_USER": config.get("PROXY_USER"),
+                "PROXY_PASS": config.get("PROXY_PASS"),
+                "PROXY_HOST": config.get("PROXY_HOST"),
+                "PROXY_PORT": config.get("PROXY_PORT"),
+            })
+            proxies = { "http": proxy_url, "https": proxy_url }
+            session.proxies.update(proxies)
+
         logger.info(f"Current User-Agent: {session.headers['User-Agent']}")
         yield session
     logger.debug("Closing requests session")
