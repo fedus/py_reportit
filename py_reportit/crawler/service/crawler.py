cc21f317157503d6a40cdcdd31c5a0ab0de94877
Federico Gentile <gentfede+git@gmail.com>
2022-04-10 13:41:08 +0200

    fix: Improvement of stop condition handling when latest report in db is also latest report online, PEP improvements

--- py_reportit/crawler/service/crawler.py
+++ py_reportit/crawler/service/crawler.py
@@ -1,25 +1,28 @@
 from __future__ import annotations
 
-import logging, sys, random
-
-from arrow import Arrow
+import logging
+import random
+import sys
 from datetime import datetime, timedelta, tzinfo
 from typing import Optional
-from sqlalchemy.orm import Session
+
+from arrow import Arrow
 from requests.models import HTTPError
+from sqlalchemy.orm import Session
 
 from py_reportit.crawler.celery.tasks import chained_crawl
-from py_reportit.shared.model.crawl_item import CrawlItem, CrawlItemState
+from py_reportit.crawler.service.photo import PhotoService
+from py_reportit.crawler.service.reportit_api import ReportItService
+from py_reportit.crawler.util.reportit_utils import extract_ids, filter_reports_by_state, \
+    generate_random_times_between, generate_time_graph, pretty_format_time
 from py_reportit.shared.model.crawl import Crawl
+from py_reportit.shared.model.crawl_item import CrawlItem, CrawlItemState
 from py_reportit.shared.model.report import Report
 from py_reportit.shared.repository.crawl import CrawlRepository
 from py_reportit.shared.repository.crawl_item import CrawlItemRepository
-from py_reportit.shared.repository.report import ReportRepository
 from py_reportit.shared.repository.meta import MetaRepository
+from py_reportit.shared.repository.report import ReportRepository
 from py_reportit.shared.repository.report_answer import ReportAnswerRepository
-from py_reportit.crawler.service.reportit_api import ReportItService
-from py_reportit.crawler.service.photo import PhotoService
-from py_reportit.crawler.util.reportit_utils import extract_ids, filter_reports_by_state, generate_random_times_between, generate_time_graph, pretty_format_time
 
 logger = logging.getLogger(f"py_reportit.{__name__}")
 
@@ -49,8 +52,15 @@ class CrawlerService:
 
     @staticmethod
     def filter_updated_reports(existing_reports: list[Report], new_reports: list[Report]) -> list[Report]:
-        get_existing_report = lambda new_report: next(filter(lambda existing_report: existing_report.id == new_report.id, existing_reports), None)
-        report_is_new_or_updated = lambda new_report: get_existing_report(new_report).updated_at < new_report.updated_at if get_existing_report(new_report) else True
+        get_existing_report = lambda new_report: next(
+            filter(lambda existing_report: existing_report.id == new_report.id, existing_reports),
+            None
+        )
+
+        report_is_new_or_updated = \
+            lambda new_report: get_existing_report(
+                new_report).updated_at < new_report.updated_at if get_existing_report(new_report) else True
+
         return list(filter(lambda report: report_is_new_or_updated(report), new_reports))
 
     def get_active_crawl(self, session: Session) -> Optional[Crawl]:
@@ -64,12 +74,12 @@ class CrawlerService:
         return crawls[0] if crawls else None
 
     def create_and_persist_new_crawl(
-        self,
-        session: Session,
-        ids_and_crawl_times: list[tuple[int, Arrow]],
-        scheduled_at: Arrow,
-        stop_at_lat: Optional[float],
-        stop_at_lon: Optional[float],
+            self,
+            session: Session,
+            ids_and_crawl_times: list[tuple[int, Arrow]],
+            scheduled_at: Arrow,
+            stop_at_lat: Optional[float],
+            stop_at_lon: Optional[float],
     ) -> Crawl:
         crawl = Crawl(
             scheduled_at=scheduled_at,
@@ -99,21 +109,27 @@ class CrawlerService:
     def set_skip_remaining_items(self, session: Session, last_processed_crawl_item: CrawlItem) -> None:
         self.crawl_item_repository.update_many(
             session,
-            { "state": CrawlItemState.SKIPPED },
+            {"state": CrawlItemState.SKIPPED},
             CrawlItem.crawl_id == last_processed_crawl_item.crawl_id,
             CrawlItem.state == CrawlItemState.WAITING
         )
 
-    def get_new_and_deleted_report_count(self, pre_crawl_ids: list[int], crawled_ids: list[int]) -> tuple[int]:
+    def get_new_and_deleted_report_count(self, pre_crawl_ids: list[int], crawled_ids: list[int]) -> tuple[int, int]:
         added_count = len(list(set(crawled_ids) - set(pre_crawl_ids)))
         removed_count = len(list(set(pre_crawl_ids) - set(crawled_ids)))
-        return (added_count, removed_count)
+        return added_count, removed_count
 
     def get_recent_reports(self, session: Session) -> list[Report]:
-        recent_reports = self.report_repository.get_by(session, Report.created_at > datetime.today() - timedelta(days=int(self.config.get("FETCH_REPORTS_OF_LAST_DAYS"))))
+        recent_reports = self.report_repository.get_by(
+            session,
+            Report.created_at > datetime.today() - timedelta(days=int(self.config.get("FETCH_REPORTS_OF_LAST_DAYS")))
+        )
 
         if not recent_reports:
-            recent_reports = self.report_repository.get_latest(session, int(self.config.get("FETCH_REPORTS_FALLBACK_AMOUNT")))
+            recent_reports = self.report_repository.get_latest(
+                session,
+                int(self.config.get("FETCH_REPORTS_FALLBACK_AMOUNT"))
+            )
 
         if not recent_reports:
             recent_reports = []
@@ -124,14 +140,22 @@ class CrawlerService:
         if immediate:
             crawl_offset_minutes = 0
         else:
-            crawl_offset_minutes = random.randint(int(self.config.get("CRAWL_FIRST_OFFSET_MINUTES_MIN")), int(self.config.get("CRAWL_FIRST_OFFSET_MINUTES_MAX")))
+            crawl_offset_minutes = random.randint(
+                int(self.config.get("CRAWL_FIRST_OFFSET_MINUTES_MIN")),
+                int(self.config.get("CRAWL_FIRST_OFFSET_MINUTES_MAX"))
+            )
 
-        crawl_duration_minutes = random.randint(int(self.config.get("CRAWL_DURATION_MINUTES_MIN")), int(self.config.get("CRAWL_DURATION_MINUTES_MAX")))
+        crawl_duration_minutes = random.randint(
+            int(self.config.get("CRAWL_DURATION_MINUTES_MIN")),
+            int(self.config.get("CRAWL_DURATION_MINUTES_MAX"))
+        )
 
         crawl_start_time = Arrow.now(self.timezone) + timedelta(minutes=crawl_offset_minutes)
         crawl_end_time = crawl_start_time + timedelta(minutes=crawl_duration_minutes)
 
-        logger.info(f"Generating {amount} crawl times from {pretty_format_time(crawl_start_time)} to {pretty_format_time(crawl_end_time)}. Offset: {crawl_offset_minutes} min, duration: {crawl_duration_minutes} min")
+        logger.info(f"Generating {amount} crawl times from {pretty_format_time(crawl_start_time)} to "
+                    f"{pretty_format_time(crawl_end_time)}. Offset: {crawl_offset_minutes} min, "
+                    f"duration: {crawl_duration_minutes} min")
         return generate_random_times_between(crawl_start_time, crawl_end_time, amount)
 
     def generate_time_graph_for_crawl(self, crawl: Crawl) -> str:
@@ -149,6 +173,9 @@ class CrawlerService:
         logger.info("Fetching existing recent reports from database ...")
         recent_reports = self.get_recent_reports(session)
 
+        closed_recent_report_ids = []
+        amount_remaining = 0
+
         if recent_reports:
             recent_ids = extract_ids(recent_reports)
             closed_recent_report_ids = extract_ids(filter_reports_by_state(recent_reports, True))
@@ -157,15 +184,28 @@ class CrawlerService:
             amount_closed = len(closed_recent_report_ids)
             amount_remaining = amount_fetched - amount_closed
 
-            logger.info(f"{amount_fetched} recent reports fetched, of which {amount_closed} are already closed, {amount_remaining} remaining")
+            logger.info(f"{amount_fetched} recent reports fetched, of which {amount_closed} are already closed, "
+                        f"{amount_remaining} remaining")
         else:
             fallback_id = int(self.config.get("FETCH_REPORTS_FALLBACK_START_ID"))
             recent_ids = [fallback_id]
             logger.info(f"No recent reports found in database, beginning crawl from fallback id {fallback_id}")
 
-        lookahead_ids = list(range(recent_ids[-1] + 1, recent_ids[-1] + 1 + int(self.config.get("FETCH_REPORTS_LOOKAHEAD_AMOUNT"))))
-        all_combined_ids = random.sample(recent_ids, len(recent_ids)) + lookahead_ids
-        relevant_combined_ids = [report_id for report_id in all_combined_ids if report_id not in closed_recent_report_ids]
+        recent_ids_without_last = recent_ids[:-1]
+        recent_ids_last_as_list = recent_ids[-1:]
+
+        lookahead_ids = list(
+            range(recent_ids[-1] + 1, recent_ids[-1] + 1 + int(self.config.get("FETCH_REPORTS_LOOKAHEAD_AMOUNT")))
+        )
+
+        # We want to keep the position of the last report in the db at the same position in case it matches the stop
+        # condition. If the last report in the db is already "closed", we currently run into a situation where the
+        # stop condition cannot be met and unnecessary crawls are performed. To be fixed / improved.
+        all_combined_ids = random.sample(
+            recent_ids_without_last, len(recent_ids_without_last)
+        ) + recent_ids_last_as_list + lookahead_ids
+
+        relevant_combined_ids = [r_id for r_id in all_combined_ids if r_id not in closed_recent_report_ids]
 
         crawl_times = self.generate_crawl_times(len(relevant_combined_ids), immediate=immediate)
 
@@ -185,10 +225,12 @@ class CrawlerService:
             logger.info(f"Setting stop condition at lat / lon: {last_lat} / {last_lon}")
 
         except HTTPError:
-            logger.warn(f"Encountered error while trying to fetch latest truncated report, not setting stop condition.", exc_info=True)
+            logger.warning(f"Encountered error while trying to fetch latest truncated report,"
+                           f"not setting stop condition.", exc_info=True)
 
         try:
-            logger.info(f"Processing {len(relevant_combined_ids)} reports, of which {amount_remaining} existing reports")
+            logger.info(f"Processing {len(relevant_combined_ids)} reports,"
+                        f"of which {amount_remaining} existing reports")
 
             logger.info("Persisting crawl planning to database ...")
 
@@ -218,6 +260,6 @@ class CrawlerService:
 
             session.commit()
 
-        except:
+        except (Exception,):
             logger.error("Unexpected error during crawl: ", sys.exc_info()[0])
             return
