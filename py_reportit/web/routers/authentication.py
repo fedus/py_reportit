4411e2b89e03c30d3d374a24c300741b1d49bff2
Federico Gentile <gentfede+git@gmail.com>
2022-04-10 14:16:51 +0200

    chore: more PEP improvements

--- py_reportit/web/routers/authentication.py
+++ py_reportit/web/routers/authentication.py
@@ -8,17 +8,19 @@ from py_reportit.shared.config.container import Container
 from py_reportit.shared.repository.user import UserRepository
 from py_reportit.web.schema.token import Token, TokenData
 from py_reportit.web.schema.user import User
-from py_reportit.web.dependencies import authenticate_user, get_current_active_user, get_session, credentials_exception, create_access_token, create_refresh_token
+from py_reportit.web.dependencies import authenticate_user, get_current_active_user, get_session, \
+    credentials_exception, create_access_token, create_refresh_token
 
 router = APIRouter(tags=["authentication"], prefix="/auth")
+
 
 @router.post("/register", status_code=201)
 @inject
 async def register(
-    username = Form(..., description="Username to be registered"),
-    password = Form(..., description="Password to be used with new account"),
-    user_repository: UserRepository = Depends(Provide[Container.user_repository]),
-    session: Session = Depends(get_session)
+        username=Form(..., description="Username to be registered"),
+        password=Form(..., description="Password to be used with new account"),
+        user_repository: UserRepository = Depends(Provide[Container.user_repository]),
+        session: Session = Depends(get_session)
 ):
     if user_repository.get_by_username(session, username):
         raise HTTPException(
@@ -29,11 +31,12 @@ async def register(
     new_user = User(username=username, password=password)
     user_repository.create(session, new_user)
 
+
 @router.post("/token", response_model=Token)
 @inject
 async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session)
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        session: Session = Depends(get_session)
 ):
     user = authenticate_user(session, form_data.username, form_data.password)
 
@@ -51,16 +54,17 @@ async def login_for_access_token(
 
     return Token(
         access_token=access_token,
-        refresh_token=refresh_token, 
+        refresh_token=refresh_token,
         token_type="bearer"
     )
+
 
 @router.post("/refresh", response_model=Token)
 @inject
 async def refresh_token(
-    grant_type: str = Form(...),
-    refresh_token: str = Form(...),
-    config: dict = Depends(Provide[Container.config]),
+        grant_type: str = Form(...),
+        refresh_token: str = Form(...),
+        config: dict = Depends(Provide[Container.config]),
 ):
     if grant_type != "refresh_token":
         raise HTTPException(
@@ -85,9 +89,10 @@ async def refresh_token(
 
     return Token(
         access_token=access_token,
-        refresh_token=refresh_token, 
+        refresh_token=refresh_token,
         token_type="bearer"
     )
+
 
 @router.get("/me", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
